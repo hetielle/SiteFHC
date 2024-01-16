3b054e027e8873193a14857174161b1c7b2b7475
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2024-01-16 17:16:52 -0300

    Adicionando rotas e instalando toastify

--- site-fhc/src/App.js
+++ site-fhc/src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Home from './ui/pages/Home';
+import Doacao from './ui/pages/Doacao';
+import Fundacao from './ui/pages/Fundacao';
+import Ouvidoria from './ui/pages/Ouvidoria'
+
 
 function App() {
   return (
     <div className="App">
+
+      <ToastContainer/>
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='*' element={<Error/>}/> 
+
+          <Route path='/' element={<Home/>}/> 
+          <Route path='/doacao' element={<Doacao/>}/>
+          <Route path='/fundacao' element={<Fundacao/>}/> 
+          <Route path='/ouvidoria' element={<Ouvidoria/>}/>
+
+        </Routes>
+      </BrowserRouter>
       
     </div>
   );
