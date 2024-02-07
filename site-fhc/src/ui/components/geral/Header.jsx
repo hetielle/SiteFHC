9356c5893237aeb1f17c8a219027a2ca0d2a9285
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2024-02-07 19:07:28 -0300

    Arrumando JS para mostrar menu selecionado

--- site-fhc/src/ui/components/geral/Header.jsx
+++ site-fhc/src/ui/components/geral/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './style/header/Header.css';
 import './style/header/HeaderSmartphone.css';
@@ -6,29 +7,36 @@ import logo from './img/logo.png';
 
 const Header = () => {
 
-    return (
-      <header>
+  const [activePage, setActivePage] = useState('');
 
-        <Link to={"/"}>
-          <img src={logo} alt="Logo"/>
-        </Link>
+  useEffect(() => {
+    const currentUrl = window.location.pathname;
+    setActivePage(currentUrl);
+  }, []);
 
-        <ul>
-          <li>
-            <Link to={"/ouvidoria"} className="link">Fale Conosco</Link>
-          </li>
-          <li>
-            <Link to={"/fundacao"} className="link">Fundação</Link>
-          </li>
-          <li>
-            <Link to={"/noticias"} className="link">Notícias</Link>
-            {/* Add dropdown menu */}
-          </li>
+  return (
+    <header>
+
+      <Link to={"/"}>
+        <img src={logo} alt="Logo"/>
+      </Link>
+
+      <ul>
+        <li>
+          <Link to={"/ouvidoria"} className={activePage === '/ouvidoria' ? 'link active-page' : 'link'}>Fale Conosco</Link>
+        </li>
+        <li>
+          <Link to={"/fundacao"} className={activePage === '/fundacao' ? 'link active-page' : 'link'}>Fundação</Link>
+        </li>
+        <li>
+          <Link to={"/noticias"} className={activePage === '/noticias' ? 'link active-page' : 'link'}>Notícias</Link>
+          {/* Add dropdown menu */}
+        </li>
           
-        </ul>
+      </ul>
 
-      </header>
-    )
-  }
+    </header>
+  )
+}
    
-  export default Header  
+export default Header  
