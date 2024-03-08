0b05e8fed6160232e1d2226cbc1f3fd25a095309
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2024-03-08 16:23:06 -0300

    Adicionando IFrame do Google Maps

--- site-fhc/src/ui/components/ouvidoria/Ouvidoria.jsx
+++ site-fhc/src/ui/components/ouvidoria/Ouvidoria.jsx
@@ -18,25 +18,34 @@ const Ouvidoria = () => {
 
         <section className="container-contatos">
 
-          <h2>Fale conosco por telefone</h2>
+          <div className="container-telefones">
 
-          <p className="bold">O que você está procurando?</p>
-          
-          <div className="container-telefone">
-            <p className="bold">Central de atendimento: </p>
-            <p>3591-1400</p>
+            <h2 className="bold">Fale conosco por telefone</h2>
+            <br/>
+
+            <div className="container-telefone">
+              <p className="bold">Central de atendimento: </p>
+              <p>3591-1400</p>
+            </div>
+
+            <p className="bold">Administração: </p>
+            <p>Ramal 430</p>
+
+            <p className="bold">Recursos Humanos: </p>
+            <p>Ramais 520/521/530</p>
+
+            <p className="bold">Comunicação: </p>
+            <p>Ramal 425</p>
           </div>
 
-          <p className="bold">Administração: </p>
-          <p>Ramal 430</p>
+          
 
-          <p className="bold">Recursos Humanos: </p>
-          <p>Ramais 520/521/530</p>
-
-          <p className="bold">Comunicação: </p>
-          <p>Ramal 425</p>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13852.188908161348!2d-51.1486605!3d-29.7762759!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95196858316903d3%3A0x21721f602a8df503!2zRnVuZGHDp8OjbyBIb3NwaXRhbCBDZW50ZW7DoXJpbw!5e0!3m2!1spt-BR!2sbr!4v1709761702279!5m2!1spt-BR!2sbr"
+          width="400" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="mapa-hospital"></iframe>
 
         </section>
+
+       
 
         <section className="container-ouvidoria">
 
