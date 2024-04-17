9836104fc653722b8124e9ae2720c5c745d57f3c
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2024-04-17 16:05:10 -0300

    Página escalas

--- site-fhc/src/ui/components/escalas/pdfs-escala/RotinaAlojamentoConjunto.jsx
+++ site-fhc/src/ui/components/escalas/pdfs-escala/RotinaAlojamentoConjunto.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../style/escala/Escala.css";
+import "../style/escala/EscalaSmartphone.css";
 import { Link } from "react-router-dom";
 import Header from "../../geral/Header";
 import Footer from "../../geral/Footer";
