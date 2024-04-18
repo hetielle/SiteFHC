7b3ddbe210c28b84c23cc1b954af002370fa352e
Hetielle Vitória de Matos <119357218+hetielle@users.noreply.github.com>
2024-04-18 19:05:18 -0300

    Página ouvidoria

--- site-fhc/src/ui/components/ouvidoria/Ouvidoria.jsx
+++ site-fhc/src/ui/components/ouvidoria/Ouvidoria.jsx
@@ -6,6 +6,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import "./style/ouvidoria/Ouvidoria.css";
 import "./style/ouvidoria/OuvidoriaSmartphone.css";
+import imgOuvidoria from "../ouvidoria/img/ouvidoria.png";
 
 const Ouvidoria = () => {
   
@@ -16,41 +17,25 @@ const Ouvidoria = () => {
 
         <Header/>
 
-        <div className="container-titulo animate__animated animate__fadeInLeft">
-          <h1 className="animate__animated animate__fadeInDown">Sua opinião é muito importante para nós!</h1>
-          <hr/>
-        </div>
-
-        <section className="container-contatos animate__animated animate__fadeInUp">
-
-          <div className="container-telefones animate__animated animate__fadeInLeft">
-
-            <h2 className="bold">Fale conosco por telefone</h2>
-            <br/>
-
-            <div className="container-telefone">
-              <p className="bold">Central de atendimento: </p>
-              <p>3591-1400</p>
+        <div className="container-grupo-ouvidoria">
+          <div className="container-left-ouvidoria">
+            <div className="container-titulo animate__animated animate__fadeInLeft">
+              <h1 className="animate__animated animate__fadeInDown">Sua opinião é muito importante para nós!</h1>
+              <hr/>
             </div>
 
-            <p className="bold">Administração: </p>
-            <p>Ramal 430</p>
+            <section className="container-contatos animate__animated animate__fadeInUp">
 
-            <p className="bold">Recursos Humanos: </p>
-            <p>Ramais 520/521/530</p>
+              <h2 className="bold">Fale conosco por telefone</h2>
+              <h2 className="animate__animated animate__fadeInUp" id="telefone">3591-1400</h2>
 
-            <p className="bold">Comunicação: </p>
-            <p>Ramal 425</p>
+            </section>
           </div>
 
-          
-
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13852.188908161348!2d-51.1486605!3d-29.7762759!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95196858316903d3%3A0x21721f602a8df503!2zRnVuZGHDp8OjbyBIb3NwaXRhbCBDZW50ZW7DoXJpbw!5e0!3m2!1spt-BR!2sbr!4v1709761702279!5m2!1spt-BR!2sbr"
-          width="400" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="mapa-hospital" className="animate__animated animate__fadeInRight"></iframe>
-
-        </section>
-
-       
+          <div className="container-right-ouvidoria animate__animated animate__fadeInRight">
+            <img src={imgOuvidoria} alt="Desenho de pessoa em ligação"/>
+          </div> 
+        </div>      
 
         <section data-aos="fade-up" className="container-ouvidoria">
 
